e22eb44368ea65cd1fc8283d03bebccd1b55256b
Martijn Klene <martijn.klene@voiceworks.com>
2018-01-05 19:24:23 +0100

    Appliction thingie has moved to infra

--- src/test/java/nl/martijnklene/api/infrastructure/ApiApplicationTests.java
+++ src/test/java/nl/martijnklene/api/infrastructure/ApiApplicationTests.java
@@ -1,16 +1,16 @@
 package nl.martijnklene.api.infrastructure;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
+@ContextConfiguration(
+        classes = {ApiApplication.class}
+)
+@WebAppConfiguration
+@TestPropertySource(locations = "classpath:application.properties")
 @SpringBootTest
 public class ApiApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
 
 }
