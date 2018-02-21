fe523d0ce47e6ff430faa9142a1328f0e05c1d7a
Martijn Klene <martijn.klene@voiceworks.com>
2018-02-21 15:04:51 +0100

    Removing unused import and swagger config now displaying the right entry points.

--- src/main/java/nl/martijnklene/api/infrastructure/configuration/SwaggerConfig.java
+++ src/main/java/nl/martijnklene/api/infrastructure/configuration/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package nl.martijnklene.api.infrastructure.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,11 +11,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("nl.martijnklene.api"))
-                .paths(PathSelectors.ant("/v1/blog"))
                 .build();
     }
 }
