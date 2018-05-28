bc66b9354c0fdc20344772f324a24ecbd49db494
Martijn Klene <martijn.klene@voiceworks.com>
2018-05-28 15:42:08 +0200

    Configuration looks OK, swagger is having issues though.

--- src/main/java/nl/martijnklene/api/infrastructure/configuration/SecurityConfig.java
+++ src/main/java/nl/martijnklene/api/infrastructure/configuration/SecurityConfig.java
@@ -1,5 +1,7 @@
 package nl.martijnklene.api.infrastructure.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,10 +9,15 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private OAuth2RestTemplate restTemplate;
 
     @Override
     public void configure(WebSecurity web) {
@@ -30,14 +37,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.OPTIONS);
     }
 
+    @Bean
+    public OpenIdConnectFilter openIdConnectFilter() {
+        return new OpenIdConnectFilter("/login", restTemplate);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .cors().and()
-                .httpBasic().disable()
-                .authorizeRequests()
-                .anyRequest().authenticated()
+        http
+            .addFilterAfter(new OAuth2ClientContextFilter(), AbstractPreAuthenticatedProcessingFilter.class)
+            .addFilterAfter(openIdConnectFilter(), OAuth2ClientContextFilter.class)
+            .cors().and()
+            .httpBasic().disable()
+            .authorizeRequests()
+            .anyRequest().authenticated()
         ;
     }
 }
