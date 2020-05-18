9f71213e13fe4fa807605a9c21a613f68f3842c4
Martijn Klene <martijn.klene@voiceworks.com>
2020-05-18 19:22:34 +0200

    Switched to auto configuring the sso stuff.

--- src/main/java/nl/martijnklene/api/infrastructure/configuration/SecurityConfig.java
+++ src/main/java/nl/martijnklene/api/infrastructure/configuration/SecurityConfig.java
@@ -1,57 +1,20 @@
 package nl.martijnklene.api.infrastructure.configuration;
 
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.*;
-import org.springframework.core.Ordered;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @Configuration
-@EnableWebSecurity
-@Order(Ordered.HIGHEST_PRECEDENCE)
-@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
-class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
-
-    @Autowired
-    public void configureGlobal(
-            AuthenticationManagerBuilder auth) throws Exception {
-
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider
-                = keycloakAuthenticationProvider();
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
-                new SimpleAuthorityMapper());
-        auth.authenticationProvider(keycloakAuthenticationProvider);
-    }
-
-    @Bean
-    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
-    }
-
-    @Bean
+@EnableOAuth2Client
+@Order(value = 0)
+class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(
-                new SessionRegistryImpl());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+    public void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST).authenticated()
                 .antMatchers(HttpMethod.PUT).authenticated()
@@ -59,6 +22,5 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.DELETE).authenticated()
                 .anyRequest()
                 .permitAll().and().csrf().disable();
-
     }
 }
--- src/main/java/nl/martijnklene/api/infrastructure/configuration/SwaggerConfig.java
+++ src/main/java/nl/martijnklene/api/infrastructure/configuration/SwaggerConfig.java
@@ -3,57 +3,82 @@ package nl.martijnklene.api.infrastructure.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.AuthorizationCodeGrantBuilder;
+import springfox.documentation.builders.OAuthBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Value("${keycloak.realm}")
+    @Value("${security.oauth2.client.client-id}")
     private String clientId;
-    @Value("${openid.tokenUri}")
+    @Value("${security.oauth2.client.access-token-uri}")
     private String tokenUri;
-    @Value("${openid.authorisationUri}")
+    @Value("${security.oauth2.client.user-authorization-uri}")
     private String authorisationUri;
-    @Value("${openid.redirectUri}")
-    private String redirectUri;
+    @Value("${security.oauth2.client.client-secret}")
+    private String clientSecret;
 
     @Bean
     public Docket api() {
-        SecurityReference securityReference = SecurityReference.builder()
-                .scopes(new AuthorizationScope[0])
-                .reference("blog_auth")
-                .build();
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(Arrays.asList(securityScheme()))
+                .securityContexts(Arrays.asList(securityContext()));
+    }
 
-        ArrayList<SecurityContext> securityContexts = newArrayList(
-                SecurityContext.builder()
-                        .securityReferences(newArrayList(securityReference))
-                        .build()
-        );
-        return new Docket(DocumentationType.SWAGGER_2)
-                .securitySchemes(newArrayList(securitySchema()))
-                .securityContexts(securityContexts)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("nl.martijnklene.api"))
+    @Bean
+    public SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder()
+                .clientId(this.clientId)
+                .clientSecret(this.clientSecret)
+                .scopeSeparator(" ")
+                .useBasicAuthenticationWithAccessCodeGrant(true)
                 .build();
     }
 
-    private OAuth securitySchema() {
-        TokenRequestEndpoint tokenRequestEndpoint = new TokenRequestEndpoint(
-                this.authorisationUri,
-                this.clientId,
-                ""
-        );
-        TokenEndpoint tokenEndpoint = new TokenEndpoint(tokenUri, "blog");
-        GrantType grantType = new AuthorizationCodeGrant(tokenRequestEndpoint, tokenEndpoint);
-        return new OAuth("blog_auth", newArrayList(), newArrayList(grantType));
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(
+                        Arrays.asList(new SecurityReference("spring_oauth", scopes())))
+                .forPaths(PathSelectors.regex("/foos.*"))
+                .build();
+    }
+
+    private SecurityScheme securityScheme() {
+        GrantType grantType = new AuthorizationCodeGrantBuilder()
+                .tokenEndpoint(new TokenEndpoint(this.tokenUri, "oauthtoken"))
+                .tokenRequestEndpoint(
+                        new TokenRequestEndpoint(this.authorisationUri, this.clientId, this.clientSecret))
+                .build();
+
+        SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
+                .grantTypes(Arrays.asList(grantType))
+                .scopes(Arrays.asList(scopes()))
+                .build();
+        return oauth;
+    }
+
+    private AuthorizationScope[] scopes() {
+        AuthorizationScope[] scopes = {
+                new AuthorizationScope("read", "for read operations"),
+                new AuthorizationScope("write", "for write operations"),
+                new AuthorizationScope("foo", "Access foo API") };
+        return scopes;
     }
 }
