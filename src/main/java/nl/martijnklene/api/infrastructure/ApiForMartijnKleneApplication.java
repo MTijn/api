75e76555d65b62fe7b02d880ebb984a97c932a88
Martijn Klene <martijn.klene@voiceworks.com>
2020-05-18 16:32:14 +0200

    Added a new property source

--- src/main/java/nl/martijnklene/api/infrastructure/ApiForMartijnKleneApplication.java
+++ src/main/java/nl/martijnklene/api/infrastructure/ApiForMartijnKleneApplication.java
@@ -6,16 +6,22 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @EnableOAuth2Sso
 @ComponentScans(
-		@ComponentScan(basePackages = "nl.martijnklene.api")
+        @ComponentScan(basePackages = "nl.martijnklene.api")
 )
 @SpringBootApplication
 @EntityScan(basePackages = {"nl.martijnklene.api", "org.axonframework.eventhandling.saga.repository.jpa", "org.axonframework.eventsourcing.eventstore.jpa"})
+@PropertySources({
+        @PropertySource(value = "classpath:application.properties"),
+        @PropertySource(value = "file:/run/secrets/blog_api_config", ignoreResourceNotFound = true)
+})
 public class ApiForMartijnKleneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiForMartijnKleneApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiForMartijnKleneApplication.class, args);
+    }
 }
